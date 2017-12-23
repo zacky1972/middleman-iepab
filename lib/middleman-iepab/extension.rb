a5cf625cac4e26c41bcf05b6154c40bbf707331c
Susumu Yamazaki <zacky1972@gmail.com>
2017-12-23 23:56:22 +0900

    change superclass

--- lib/middleman-iepab/extension.rb
+++ lib/middleman-iepab/extension.rb
@@ -3,14 +3,18 @@ require 'middleman-core'
 
 # Extension namespace
 module Middleman
-  class IEPABExtension < ::Middleman::Extensions::ExternalPipeline
+  class IEPABExtension < ::Middleman::Extension
     self.supports_multiple_instances = true
-    
+
     option :name, nil, 'The name of the pipeline', required: true
     option :command, nil, 'The command to initialize', required: true
     option :source, nil, 'Path to merge into sitemap', required: true
     option :latency, 0.25, 'Latency between refreshes of source'
     option :disable_background_execution, false, "Don't run the command in a separate background thread"
+
+    def initialize(app, config={}, &block)
+      super
+    end
 
     def after_configuration
     end
